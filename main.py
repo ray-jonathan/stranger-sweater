36233dba159b689e18ebe1fe43f3d89a47fc9cd1
Jonathan Ray <jonathanray17@gmail.com>
2019-11-13 11:23:25 -0500

    testing api

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from shift import Shifter
 import requests
 
 
-shift = Shifter()
-should_prompt = True
-while should_prompt:
-    shift.spell(input('What to say? ').upper())
-    if input("Again? y/n ").upper() != "Y":
-        print("Bye!")
-        should_prompt = False
+# shift = Shifter()
+# should_prompt = True
+# while should_prompt:
+#     shift.spell(input('What to say? ').upper())
+#     if input("Again? y/n ").upper() != "Y":
+#         print("Bye!")
+#         should_prompt = False
+r = requests.get('http://sweater.jonathan-ray.com/')
+parsed = r.json()
+print(parsed["msg"])
 exit()
