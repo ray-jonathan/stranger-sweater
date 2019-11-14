c185bfb0bb6243d6307ddae68440d72f667aaf4c
Jonathan Ray <jonathanray17@gmail.com>
2019-11-14 08:51:37 -0500

    imperative code now calls itself

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from shift import Shifter
 import requests
 
 
-shift = Shifter()
-# should_prompt = True
-# while should_prompt:
-#     shift.spell(input('What to say? ').upper())
-#     if input("Again? y/n ").upper() != "Y":
-#         print("Bye!")
-#         should_prompt = False
-r = requests.get('http://sweater.jonathan-ray.com/')
-parsed = r.json()
-shift.spell(parsed["msg"])
+def main(text="ho ho ho "):
+    shift.spell(text)
+    r = requests.get('http://sweater.jonathan-ray.com/')
+    parsed = r.json()
+    text = parsed["msg"]
+    main(text)
+
+
+main()
+
 exit()
