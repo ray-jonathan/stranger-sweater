99ea010c832cf97e9273806fb8f77fb0ad18ed17
Jonathan Ray <jonathanray17@gmail.com>
2019-11-15 10:18:02 -0500

    updates main with number convering

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from shift import Shifter
 import requests
+from regex import convert
 
 
 def main(text="ho ho ho "):
+    text = convert(text)
     shift.spell(text)
     r = requests.get('https://sweater.jonathan-ray.com/')
     parsed = r.json()
