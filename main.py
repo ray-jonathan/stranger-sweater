4887f80d1bba0b8b5188ce8874c9a7385a88bb25
Jonathan Ray <jonathanray17@gmail.com>
2019-11-15 17:29:41 -0500

    edits url

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ shift.radiate()
 def main(text="ho ho ho "):
     text = convert(text)
     shift.spell(text)
-    r = requests.get('https://sweater.jonathan-ray.com/')
+    r = requests.get('https://sweater.jonathan-ray.com/msg')
     parsed = r.json()
     text = parsed["msg"]
     main(text)
